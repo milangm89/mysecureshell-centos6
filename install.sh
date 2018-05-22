c26ca3e59b567f6cfa520aebe90cffc85b9396e2
Milan George <milangm89@gmail.com>
2018-05-22 12:54:43 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,44 +1,48 @@
 #!/bin/bash
 # Author : Milan George
+# version 1.01
 
 # Copying mysecureshell repo file
 cp -r ./mysecureshell.repo /etc/yum.repos.d/mysecureshell.repo
 
-# Installing mysecureshell
-yum install -y mysecureshell
+# Installing mysecureshell and necessary packages
+echo "============================"
+echo "Installing necessay packages"
+echo "============================"
+yum install -y mysecureshell expect
 
 # Making required changes in ssh config file
-echo " Making required changes in sshd config file \n"
-echo " Copying current ssh config file as backup \n"
-cp /etc/ssh/sshd_config /etc/ssh/sshd_config_orig
+echo "============================"
+echo -e "Making required changes in sshd config file \n"
+echo -e "Copying current sshd config file as backup \n"
+echo "============================"
+#cp /etc/ssh/sshd_config /etc/ssh/sshd_config_orig
 cp /etc/ssh/sshd_config /etc/ssh/sshd_config-`date +"%Y%m%d_%H%M%S"`
 cp -rf ./sshd_config /etc/ssh/sshd_config
 
-#sed -i '/Subsystem/s/^/#/g' /etc/ssh/sshd_config
-#echo "Subsystem sftp /bin/MySecureShell -c sftp-server" >> sshd_configsed -i '/Subsystem/s/^/#/g' sshd_config
-
-#l=`grep -n "AddressFamily" /etc/ssh/sshd_config | awk '{print $1}' | cut -d : -f 1`
-
-echo "Copying sftp config file . . \n"
-cp /etc/ssh/sftp_config /etc/ssh/sftp_config_orig
+echo "============================"
+echo -e "Copying sftp config file . . \n"
+echo "============================"
+#cp /etc/ssh/sftp_config /etc/ssh/sftp_config_orig
 cp /etc/ssh/sftp_config /etc/ssh/sftp_config-`date +"%Y%m%d_%H%M%S"`
 cp -rf ./sftp_config /etc/ssh/sftp_config
 
-echo "Creating sftp group and user. . \n"
+echo "============================"
+echo -e "Creating sftp group and user. . \n"
+echo "============================"
 groupadd sftp_users
-useradd -m -d /var/www/html -s /bin/MySecureShell ftpuser
-
-echo "Installing mkpasswd"
-yum install expect -y
+useradd -m -d /var/www/html -s /bin/MySecureShell sftpuser
 
 pwd_sftp=`mkpasswd -l 15`
-echo "$pwd_sftp" | passwd ftpuser --stdin
+echo "$pwd_sftp" | passwd sftpuser --stdin
 #echo "ftpuser:$pwd_sftp" | chpasswd 
 
 echo "Restarting services"
 /etc/init.d/sshd restart
 
+echo "============================"
 echo "Changing ownership of /var/www/html to apache"
-chown -R apache:apache /var/www/html
+echo "============================"
+chown -R apache:apache /var/www/html/
 
-echo " The username is : ftpuser and the password is : $pwd_sftp "
+echo -e " The username is : sftpuser \nand the password is : $pwd_sftp "
